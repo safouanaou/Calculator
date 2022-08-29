4cdb61308a90c30f6b7710e2e41bd4c73e88c604
safouanaou <aouezgharsafouan@gmail.com>
2022-08-29 23:51:07 +0100

    added display elements in index and removed them from script.

--- script.js
+++ script.js
@@ -1,57 +1,113 @@
 // selecting DOM elements
 const numbers = document.querySelectorAll('.numbers');
+const screen = document.querySelector('.screen');
 const display = document.getElementById('result');
 const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
+const firstNumText = document.getElementById('firstNum');
+const secondNumText = document.getElementById('secondNum');
+const resultValue = document.getElementById('resultValue');
 
-// Operate function
-const operate = function(num1, operator, num2){
-
-    // operations
-const add = function(num1, num2){
-    return num1+num2;
-}
-
-const subtract = function(num1, num2){
-    return num1-num2;
-}
-
-const multiply = function(num1, num2){
-    return num1*num2;
-}
-
-
-const divide = function(num1, num2){
-    return num1/num2;
-}
-
-
-    if(operator == '+'){
-        return add(num1, num2);
-    }else if(operator == '-'){
-        return subtract(num1, num2);
-    }else if(operator == '*'){
-        return multiply(num1, num2);
-    }else if(operator == '/'){
-        return divide(num1, num2);
-    }
-}
-
-operate(1,'+', 2)
 
 
 // Variables
-let displayValue = 0;
+let firstNumber = 0;
+let secondNumber = 0;
+let resultNumber = 0;
+let operateur = '';
 
 
-// Numbers' display functions
-numbers.forEach((number)=>{
-    number.addEventListener('click', ()=>{
-        displayValue = parseInt(number.textContent);
-        const firstNum = document.createElement('p');
-        firstNum.textContent = displayValue;
-        display.appendChild(firstNum);
+    // Numbers' display functions
+    numbers.forEach((number)=>{
+        number.addEventListener('click', ()=>{
+            firstNumText.textContent += number.textContent;
+            console.log(screen)
+
+
+
+        })
+    })
+
+    operators.forEach((operator)=>{
+        operator.addEventListener('click', ()=>{
+            firstNumber = parseInt(firstNumText.textContent);
+            operateur = operator.textContent;
+            const operate = function(num1, operator, num2){
+                    
+                num1 = firstNumber;
+                num2 = secondNumber;
+                operator = operateur;
+            
+            }
+
+
+                // the second number button is a local function, it works only when operator button is initiated
+
+                numbers.forEach((number)=>{
+                    number.addEventListener('click', ()=>{
+                    secondNumText.textContent += number.textContent;
+                    secondNumber = parseInt(secondNumText.textContent);
+                    firstNumText.remove();
+
+
+
+                    
+        })
+    })
+
+
+    equals.addEventListener('click', ()=>{
+
+        const operate = function(num1, operator, num2){
+    
+            num1 = firstNumber;
+            num2 = secondNumber;
+            operator = operateur;
+        
+            // operations
+            const add = function(num1, num2){
+            return num1+num2;
+            }   
+        
+            const subtract = function(num1, num2){
+            return num1-num2;
+            }
+        
+            const multiply = function(num1, num2){
+            return num1*num2;
+            }
+        
+        
+            const divide = function(num1, num2){
+            return num1/num2;
+            }
+        
+            if(operator == '+'){
+                return add(num1, num2);
+            }else if(operator == '-'){
+                return subtract(num1, num2);
+            }else if(operator == '*'){
+                return multiply(num1, num2);
+            }else if(operator == '/'){
+                return divide(num1, num2);
+            }
+        }
+        resultNumber = operate();
+    
+        secondNumText.remove()
+        resultValue.textContent = resultNumber;
+        // displaying result
+        
+        
     })
 })
+
+})
+
+
+
+
+
 
 
 
